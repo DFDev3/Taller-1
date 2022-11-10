657cc6cad0574880eefbaa6c666baad8a3968f38
DFDev3 <104524791+DFDev3@users.noreply.github.com>
2022-11-10 07:47:29 -0500

    Update Taller18.py

--- Taller18.py
+++ Taller18.py
@@ -23,8 +23,8 @@ def MODELOEXP(x=list,y=list):
         coefficients = np.polyfit(x, log_y, 1)
         return coefficients
 def ECUACIONES(x,y):
-    log_x = np.log(x,10)
-    log_y = np.log(y,10)
+    log_x = np.log10(x)
+    log_y = np.log10(y)
     coefficients = np.polyfit(log_x,log_y,1)
     return coefficients
 def RAZONES(x,y):
@@ -34,7 +34,8 @@ def RAZONES(x,y):
          x_inv.append(1/x[i])
          y_inv.append(1/y[i])
          coefficients = np.polyfit(x_inv,y_inv,1)
-         return coefficients
+         print(coefficients)
+    return coefficients
  
 x=[1,3,5,7,9,11,13,15]
 y=[2.1,3.2,3.8,4,4.2,4.4,4.5,4.7]
@@ -43,7 +44,8 @@ ynp=np.array(x)
 print("X    -   Y\n")
 for i in range(len(x)):
     print(f"{x[i]}    -   {y[i]}")
-print(f"MÍNIMOS CUADRADOS: y = {REGRESION[0]}x+({REGRESION[1]})\n")
-print(f"MODELO EXPONECIAL: y = {MODELOEXP(xnp,ynp)[0]}x+({MODELOEXP(xnp,ynp)[1]})")
-print(f"ECUACIONES DE POTENCIA: y = {ECUACIONES(xnp,ynp)[0]}x+({ECUACIONES(xnp,ynp)[1]}")
-print(f"RAZÓN DE CAMBIO: y = y = {RAZONES(x,y)[0]}x+({RAZONES(x,y)[1]}")
+
+    print(f"MÍNIMOS CUADRADOS: y = {REGRESION(x,y)[0]}x+({REGRESION(x,y)[1]})\n")
+    print(f"MODELO EXPONECIAL: y = {MODELOEXP(xnp,ynp)[0]}x+({MODELOEXP(xnp,ynp)[1]})")
+    print(f"ECUACIONES DE POTENCIA: y = {ECUACIONES(xnp,ynp)[0]}x+({ECUACIONES(xnp,ynp)[1]}")
+    print(f"RAZÓN DE CAMBIO: y = y = {RAZONES(x,y)[0]}x+({RAZONES(x,y)[1]}")
